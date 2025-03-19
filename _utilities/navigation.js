cd18845889fcfe65dfde8359a0120a1bc2418f1e
Brooke Bryan <brooke@bajb.net>
2025-03-19 22:06:42 +0000

    frontMatter may not exist

--- _utilities/navigation.js
+++ _utilities/navigation.js
@@ -3,17 +3,15 @@ import fs from 'fs';
 // This file is used to generate the navigation for the site. It is used in the _includes/header.html file.
 // This file will look through the pages directory and generate a list of links based on the front matter of each page.
 // If the front matter includes draft: true, the page will not be included in the navigation.
-const getNavigation = () =>
-{
+const getNavigation = () => {
   const navigation = [];
   const pagesDirectory = 'pages';
 
   // loop through the pages directory for any other directories (section titles)
   const subdirectories = fs.readdirSync(pagesDirectory, {withFileTypes: true})
-    .filter(dirent => dirent.isDirectory());
+                           .filter(dirent => dirent.isDirectory());
 
-  subdirectories.forEach(subdirectory =>
-  {
+  subdirectories.forEach(subdirectory => {
     const humanReadable = subdirectory.name.replace(/-/g, ' ').replace(/\b\w/g, l => l.toUpperCase());
 
     const section = {
@@ -23,17 +21,16 @@ const getNavigation = () =>
 
     // loop through the files in each directory
     const files = fs.readdirSync(`${pagesDirectory}/${subdirectory.name}`, {withFileTypes: true})
-      .filter(dirent => dirent.isFile());
+                    .filter(dirent => dirent.isFile());
 
-    files.forEach(file =>
-    {
+    files.forEach(file => {
       const page = fs.readFileSync(`${pagesDirectory}/${subdirectory.name}/${file.name}`, 'utf8');
       const frontMatter = page.split('---')[1];
-      const draft = frontMatter.includes('draft: true');
+      const draft = frontMatter ? frontMatter.includes('draft: true') : false;
       // title should be humanized filename
       const title = file.name.replace(/-/g, ' ')
-        .replace('.md', '')
-        .replace(/\b\w/g, l => l.toUpperCase());
+                        .replace('.md', '')
+                        .replace(/\b\w/g, l => l.toUpperCase());
 
       let url = `/${subdirectory.name}/${file.name.replace('.md', '')}`;
       // if filename is the same as the directory name, remove the filename from the url
@@ -45,9 +42,9 @@ const getNavigation = () =>
       if(!draft)
       {
         section.items.push({
-          'name': title,
-          'url':  url
-        });
+                             'name': title,
+                             'url':  url
+                           });
       }
     });
 
